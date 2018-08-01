27951ea72569dc35a6f9c36fd035be024ecfc275
sxpc722 <sxpc722@aliyun.com>
2018-08-02 01:00:41 +0800

    modify regex for array & object

--- src/type_info.c
+++ src/type_info.c
@@ -6,72 +6,73 @@
  */
 #include "type_info.h"
 
-const struct TYPE_INF T_INF_INT={
+const TypeInf T_INF_INT={
 		.name="int",
 		.type=INT,
 		.size={sizeof(int)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_UINT={
+const TypeInf T_INF_UINT={
 		.name="unsigned int",
 		.type=UINT,
 		.size={sizeof(unsigned int)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_ULONG={
+const TypeInf T_INF_ULONG={
 		.name="unsigned long",
 		.type=ULONG,
 		.size={sizeof(unsigned long)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_ULLONG={
+const TypeInf T_INF_ULLONG={
 		.name="unsigned long long",
 		.type=ULLONG,
 		.size={sizeof(unsigned long long)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_LONG={
+const TypeInf T_INF_LONG={
 		.name="long",
 		.type=LONG,
 		.size={sizeof(long)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_LLONG={
+const TypeInf T_INF_LLONG={
 		.name="long long",
 		.type=LLONG,
 		.size={sizeof(long long)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_FLOAT={
+const TypeInf T_INF_FLOAT={
 		.name="float",
 		.type=FLOAT,
 		.size={sizeof(float)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_DOUBLE={
+const TypeInf T_INF_DOUBLE={
 		.name="double",
 		.type=DOUBLE,
 		.size={sizeof(double)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_LDOUBLE={
+const TypeInf T_INF_LDOUBLE={
 		.name="long double",
 		.type=LDOUBLE,
 		.size={sizeof(long double)},
 		.subObjInfo=(void*)(0)
 };
 
-const struct TYPE_INF T_INF_BOOLEAN={
+const TypeInf T_INF_BOOLEAN={
 		.name="boolean",
 		.type=BOOLEAN,
 		.size={sizeof(char)},
 		.subObjInfo=(void*)(0)
 };
+
--- src/type_info.h
+++ src/type_info.h
@@ -10,7 +10,7 @@
 #include <stddef.h>
 typedef struct OBJECT_INF ObjectInfo;
 
-struct TYPE_INF{
+typedef struct TYPE_INF{
 	const char* name;
 	const enum {
 		STRING = 1,
@@ -31,38 +31,53 @@ struct TYPE_INF{
 	const size_t size[2];
 	//the *object int objectinfo array is the offset for attributes of STRUCT
 	const ObjectInfo* const subObjInfo;
-};
+}TypeInf;
+
 
 struct OBJECT_INF{
 	const char* const name;
-	const struct TYPE_INF* typeInf;
+	const TypeInf* typeInf;
 	//base address for STRUCT type
 	void * offset; // when it is used as offset address , the type is int.
 };
 
 // init ObjectInfo
-#define O(na,type,offs){\
+#define O(na,ty,offs){\
 			.name=#na,\
-			.typeInf=&T_INF_##type,\
+			.typeInf=&ty,\
 			.offset=(void*)(offs)\
 		}
+
 //init TYPE_INF
-#define	T(na,ty,siz,...) {\
+#define TG(na,ty,siz_ty,sub_el_num,...) (TypeInf){\
 		.name=#na,\
 		.type=ty,\
-		.size={sizeof(na),siz},\
+		.size={siz_ty,sub_el_num},\
 		.subObjInfo=(const ObjectInfo*)&(ObjectInfo[]){__VA_ARGS__}\
 }
 
-extern const struct TYPE_INF T_INF_INT;
-extern const struct TYPE_INF T_INF_UINT;
-extern const struct TYPE_INF T_INF_ULONG;
-extern const struct TYPE_INF T_INF_ULLONG;
-extern const struct TYPE_INF T_INF_LONG;
-extern const struct TYPE_INF T_INF_LLONG;
-extern const struct TYPE_INF T_INF_FLOAT;
-extern const struct TYPE_INF T_INF_DOUBLE;;
-extern const struct TYPE_INF T_INF_LDOUBLE;
-extern const struct TYPE_INF T_INF_BOOLEAN;
+//for Struct
+#define	T(na,attr_num,...) \
+		TG(na,OBJECT,sizeof(na),attr_num,__VA_ARGS__)
+
+//for Array
+#define T_ARRAY(el_type_name,el_num,el_type) \
+		TG(el_type_name##el_num,ARRAY,sizeof(el_type_name)*el_num,el_num,\
+				O(el_type_name,el_type,0))\
+//for String
+#define T_STRING(length)\
+		TG(char##length,STRING,sizeof(char)*length,length)
+
+//base type info
+extern const TypeInf T_INF_INT;
+extern const TypeInf T_INF_UINT;
+extern const TypeInf T_INF_ULONG;
+extern const TypeInf T_INF_ULLONG;
+extern const TypeInf T_INF_LONG;
+extern const TypeInf T_INF_LLONG;
+extern const TypeInf T_INF_FLOAT;
+extern const TypeInf T_INF_DOUBLE;;
+extern const TypeInf T_INF_LDOUBLE;
+extern const TypeInf T_INF_BOOLEAN;
 
 #endif /* TYPE_INFO_H_ */
