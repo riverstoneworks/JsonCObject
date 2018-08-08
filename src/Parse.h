0c489402e516051bd50d8ae003d14bfb64f865af
sxpc722 <sxpc722@aliyun.com>
2018-08-08 23:45:35 +0800

    add support to transform from string to numeric with strtoX function

--- src/Parse.h
+++ src/Parse.h
@@ -8,7 +8,14 @@
 #ifndef PARSE_H_
 #define PARSE_H_
 #include "type_info.h"
-
+/*
+ * Before this, set locate LC_CTYPE "*.UTF-8" for Unicode char transform between utf-8 and utf-16.
+ * e.g. UTF-16 char "\u040f" in JSON string
+ *
+ * 	if(!strstr(setlocale(LC_CTYPE, NULL),"UTF-8"))
+		if(!strstr(setlocale(LC_CTYPE, ""),"UTF-8"))
+			setlocale(LC_CTYPE, "en_US.UTF-8");
+ * */
 
 extern int json_cto_struct_init();
 extern int json_cto_struct(char* , ObjectInfo*);
